c1b048ecb5405e69cc964c95de4fbc8f0c1adb5f
Michał Ostrowiecki <6361@bp-itaka.net>
2023-04-06 14:51:31 +0200

    ddd

--- VCSBasics/Program.cs
+++ VCSBasics/Program.cs
@@ -6,7 +6,7 @@ namespace VCSBasics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hebnmbmbmllo World!");
+            Console.WriteLine("Hebnmmbnmbmbbmbmllo World!");
         }
     }
 }
